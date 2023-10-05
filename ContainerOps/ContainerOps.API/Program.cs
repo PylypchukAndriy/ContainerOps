10cfa54d68451fc910b824f1139c714692b845e8
Andriy Pylypchuk <andriy.pylypchuk@inventify.com>
2023-10-05 14:24:43 +0300

    Set up Swagger and main settings

--- ContainerOps/ContainerOps.API/Program.cs
+++ ContainerOps/ContainerOps.API/Program.cs
@@ -1,6 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
 app.MapGet("/", () => "Hello World!");
+
+app.UseHttpsRedirection();
 
 app.Run();
